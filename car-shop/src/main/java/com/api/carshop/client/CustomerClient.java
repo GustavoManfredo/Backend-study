97deb23b281981049cf255a07f819132c9e0302c
GustavoManfredo <work.manfredo@gmail.com>
2023-01-04 19:19:04 -0800

    Teste de integração

--- car-shop/src/main/java/com/api/carshop/client/CustomerClient.java
+++ car-shop/src/main/java/com/api/carshop/client/CustomerClient.java
@@ -9,11 +9,12 @@ import org.springframework.web.bind.annotation.*;
 @FeignClient(name = "customerClient", url = "${url}")
 public interface CustomerClient {
 
-    @GetMapping
-    @ResponseStatus(HttpStatus.OK)
-    ResponseEntity<CustomerDto> getCustomerById(@RequestParam("id") Long id);
-
     @PostMapping("/customer")
     ResponseEntity<CustomerDto> saveCustomer(@RequestBody CustomerDto customerDto);
-
+    @GetMapping("/customer/{id}")
+    ResponseEntity<CustomerDto> getCustomerById(@PathVariable(value = "id") Long id);
+    @PutMapping("/customer/{id}")
+    ResponseEntity<CustomerDto> updateCustomer(@PathVariable(value = "id")Long id, @RequestBody CustomerDto customerDto);
+    @DeleteMapping("/customer/{id}")
+    ResponseEntity<String> deleteCustomer(@PathVariable(value = "id") Long id);
 }
--- car-shop/src/test/java/com/api/carshop/controllers/CustomerControllerTest.java
+++ car-shop/src/test/java/com/api/carshop/controllers/CustomerControllerTest.java
@@ -2,13 +2,11 @@ package com.api.carshop.controllers;
 
 import com.api.carshop.client.CustomerClient;
 import com.api.carshop.dto.CustomerDto;
-import com.api.carshop.services.CustomerService;
-import org.aspectj.lang.annotation.Before;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import com.api.carshop.models.CustomerModel;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -16,30 +14,66 @@ import static org.junit.jupiter.api.Assertions.*;
 public class CustomerControllerTest {
     @Autowired
     CustomerClient customerClient;
-
+    CustomerDto customerDto;
+    CustomerModel customerEntity;
+    CustomerDto customerDtoPut;
 
     @BeforeEach
-    public CustomerDto customerDto = CustomerDto.builder()
-            .name("Dev")
-            .cpf("24350386076")
-            .cnpj("90660871000124")
-            .phone("11946313126")
-            .email("lorenzo.igor.rezende@integrasjc.com.br")
-            .address("Rua Acará, 693")
-            .city("Bragança Paulista")
-            .state("SP")
-            .pin("12913066")
-            .build();
+    public void setup(){
+        System.out.println("#----Setting up customerDto and CustomerModel----#");
+        customerDto = CustomerDto.builder()
+                .name("Dev")
+                .cpf("24350386076")
+                .cnpj("90660871000124")
+                .phone("11946313126")
+                .email("lorenzo.igor.rezende@integrasjc.com.br")
+                .address("Rua Acará, 693")
+                .city("Bragança Paulista")
+                .state("SP")
+                .pin("12913066")
+                .build();
 
+        customerEntity = CustomerModel.builder()
+                .name("Dev")
+                .cpf("24350386076")
+                .cnpj("90660871000124")
+                .phone("11946313126")
+                .email("lorenzo.igor.rezende@integrasjc.com.br")
+                .address("Rua Acará, 693")
+                .city("Bragança Paulista")
+                .state("SP")
+                .pin("12913066")
+                .build();
 
-    @AfterEach
-
-
+        customerDtoPut = CustomerDto.builder()
+                .name("DevTeste2")
+                .cpf("35461497187")
+                .cnpj("93330871110124")
+                .phone("11946317172")
+                .email("devPutTeste@integrasjc.com.br")
+                .address("Rua Acaraje, 693")
+                .city("Osasco")
+                .state("SP")
+                .pin("12915056")
+                .build();
+    }
     @Test
-    public void shouldSaveCustomer(){
-        var result = customerClient.saveCustomer(customerDto);
+    public void shouldDoCRUD(){
 
-        assertNotNull(result);
+        var saveCustomer = customerClient.saveCustomer(customerDto);
+        customerEntity.setId(saveCustomer.getBody().getId());
+        assertEquals(customerEntity.getCpf(), saveCustomer.getBody().getCpf());
+
+        var getCustomerById = customerClient.getCustomerById(customerEntity.getId());
+        assertEquals(customerEntity.getCpf(), getCustomerById.getBody().getCpf());
+
+        var putCustomer = customerClient.updateCustomer(customerEntity.getId(), customerDtoPut);
+        assertEquals(customerEntity.getId(), putCustomer.getBody().getId());
+        assertNotEquals(customerEntity.getName(), putCustomer.getBody().getName());
+
+        var deleteCustomer = customerClient.deleteCustomer(customerEntity.getId());
+        assertEquals(HttpStatus.OK, deleteCustomer.getStatusCode());
+        assertEquals("Customer deleted", deleteCustomer.getBody());
     }
 
 }
